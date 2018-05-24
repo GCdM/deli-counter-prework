75213444714045279ceeed6b28f315e3e28df7a1
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 20:21:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    serving = line.shift
+    puts "Currently serving #{serving}."
+  end
