399a01522d9d5b627f1671611b6c8b5eae6051cb
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 20:12:44 +0000

    automatically backed up by learn

---
+++ deli_counter.rb
@@ -0,0 +1,12 @@
+katz_deli = []
+
+def line
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    lineup = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      lineup += " #{index + 1} #{name}"
+    end
+  end
+end
