337583f60e47f5d292b786b2577168ff00a942d7
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 20:20:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(line, name)
   line << name
-  puts "Welcome, #{name}. You are number #{line.length}."
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end
